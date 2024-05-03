8104071d69ed88869bc8f5ca920efff22d79b059
Estela Castagna <estelacastagnadds@gmail.com>
2024-05-03 10:53:04 -0300

    Segundo commit

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,24 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\Site\HomeController;
+use App\Http\Controllers\Site\BlogController;
+use App\Http\Controllers\Site\ContactController;
+use App\Http\Controllers\Site\CategoryController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('inicio');
+Route::namespace(value:'Site')->group(function(){
+    Route::get('/', [HomeController::class, '__invoke'])->name('inicio');
+
+    Route::get('produtos', [CategoryController::class, 'index'])->name('categorias');
+    Route::get('produtos/{slug}', [CategoryController::class, 'show'])->name('categoria_produtos');
+
+    Route::get('blog', [BlogController::class, '__invoke'])->name('blog');
+
+    Route::view('sobre', 'site.sobre');
+
+    Route::get('contato',[ContactController::class, 'index'])->name('contato');
+    Route::post('contato',[ContactController::class, 'form'])->name('form_contato');
 });
 
 Route::get('/dashboard', function () {
